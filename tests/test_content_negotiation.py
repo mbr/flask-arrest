46070dd697fa18239cd751e608dddb6c422c7d07
Marc Brinkmann <git@marcbrinkmann.de>
2014-01-20 13:57:48 -0800

    Renamed test functions for less confusion.

--- tests/test_content_negotiation.py
+++ tests/test_content_negotiation.py
@@ -31,23 +31,23 @@ def client(app, api):
     return app.test_client()
 
 
-def test_no_content_type(client):
+def test_no_incoming_type(client):
     assert client.post('/accepts-foo/').status_code == 415
 
 
-def test_acceptable_content_type(client):
+def test_acceptable_incoming_type(client):
     assert client.post('/accepts-foo/',
                        headers={'Content-Type': 'application/foo'}
                        ).status_code == 200
 
 
-def test_unacceptable_content_type(client):
+def test_unacceptable_incoming_type(client):
     assert client.post('/accepts-foo/',
                        headers={'Content-Type': 'application/bar'}
                        ).status_code == 415
 
 
-def test_get_content_type_with_empty_bodies(client):
+def test_get_incoming_type_with_empty_bodies(client):
     assert client.get('/accepts-foo/').status_code == 200
     assert client.get('/accepts-foo/',
                       headers={'Content-Type': 'application/foo'}
