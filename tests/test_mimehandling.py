f3073565af45abfb409e7050bfc015e69c3c8cb5
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-03 13:24:23 +0100

    PEP8 fixes in  test_mimehandling.py.

--- tests/test_mimehandling.py
+++ tests/test_mimehandling.py
@@ -11,45 +11,44 @@ def apibp():
 
 
 def test_default_is_json(apibp):
-    assert apibp.incoming.get_mimetypes() == set(['application/json'])
+    assert apibp.incoming.get_mimetypes() == {'application/json'}
 
 
 def test_default_for_arbitrary_endpoint_is_json(apibp):
-    assert apibp.incoming.get_mimetypes('some-endpoint') ==\
-        set(['application/json'])
+    assert apibp.incoming.get_mimetypes('some-endpoint') == \
+        {'application/json'}
 
 
 def test_setting_default_mimetypes(apibp):
     apibp.incoming.set_mimetypes(['application/x', 'application/y'])
 
-    assert apibp.incoming.get_mimetypes() ==\
-        set(['application/x', 'application/y'])
+    assert apibp.incoming.get_mimetypes() == \
+        {'application/x', 'application/y'}
 
 
 def test_add_default_mimetypes(apibp):
     apibp.incoming.add_mimetype('application/new')
 
-    assert apibp.incoming.get_mimetypes() ==\
-        set(['application/json', 'application/new'])
+    assert apibp.incoming.get_mimetypes() == \
+        {'application/json', 'application/new'}
 
 
 def test_arbitrary_endpoint_mimetype_default(apibp):
     assert apibp.incoming.get_mimetypes('some_nonexisting_endpoint') ==\
-        set(['application/json'])
+        {'application/json'}
 
 
 def test_adding_to_endpoint(apibp):
     apibp.incoming.add_mimetype('application/z', 'endpointx')
 
-    assert apibp.incoming.get_mimetypes('endpointx') ==\
-        set(['application/json', 'application/z'])
+    assert apibp.incoming.get_mimetypes('endpointx') == \
+        {'application/json', 'application/z'}
 
 
 def test_disabling_default_acceptance(apibp):
     apibp.incoming.set_mimetypes(['application/only'], 'endpointy')
 
-    assert apibp.incoming.get_mimetypes('endpointy') ==\
-        set(['application/only'])
+    assert apibp.incoming.get_mimetypes('endpointy') == {'application/only'}
 
 
 def test_adding_none_allowed_for_endpoints(apibp):
